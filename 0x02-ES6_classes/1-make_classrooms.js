7a478d6b7f6d28f512c47ea87896e560bd0e3fe7
saadsgh123 <sdsghouri@gmail.com>
2024-10-03 20:07:28 +0100

    task 1 try 2 🥸

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,10 @@
-export default class ClassRoom {
-  constructor(_maxStudentsSize) {
-    this._maxStudentsSize = _maxStudentsSize;
-  }
+import ClassRoom from "./0-classroom.js";
 
-  initializeRooms(){
+export default function initializeRooms(){
     return [
         new ClassRoom(19),
         new ClassRoom(20),
         new ClassRoom(34),
     ]
   }
-}
+
