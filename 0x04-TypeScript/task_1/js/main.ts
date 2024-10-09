2052bf1d1ad28d9a6e188c59d30922fecf16839b
saadsgh123 <sdsghouri@gmail.com>
2024-10-09 15:46:53 +0100

    task 1 try 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,30 @@
-interface Teacher {
+export default interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
     [key: string]: any;
+}
+
+function printTeacher(firstname:string, lastname:string) : string {
+    return firstname + lastname
+}
+
+class StudentClass {
+    private firstname: string;
+    private lastname: string;
+
+    constructor(firstname:string, lastname:string) {
+        this.firstname = firstname;
+        this.lastname = lastname
+    }
+
+    workOnHomework() {
+        return "Currently Working"
+    }
+
+    displayName() {
+        return this.firstname
+    }
 }
